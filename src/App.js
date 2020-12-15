5952b5713580c9f08c4954b68b102bab6ad7dd6c
Gisell Mazo <gisellmazo@gmail.com>
2020-12-15 17:06:03 -0500

    Pantalla inicial lista con enrutamiento y creación de pantalla checklist en proceso

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 
 import WelcomeView from './components/contents/section-1/WelcomeView'
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import SectionChecklist from './components/contents/sectionChecklist/SectionChecklist'
+import NewTask from './components/contents/sectionChecklist/ButtonOptions/NewTask'
 
-function App() {
-  return (
+
+export default function App() {
+  return (<Router>
     <div className="App">
-      <WelcomeView/>
+      <Switch>
+        
+        <Route path="/Checklist" exact>
+          <SectionChecklist/>
+        </Route>
+
+        <Route path="/" exact>
+          <WelcomeView/>
+        </Route>
+
+      </Switch>
+      
     </div>
-    
+
+    </Router>
   );
 }
 
-export default App;
