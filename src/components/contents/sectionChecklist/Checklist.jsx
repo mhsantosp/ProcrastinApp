2d1990c329893d480df2f6c177d539d498750626
Maria Helena Santos Pineda <msantospineda1@gmail.com>
2021-01-04 21:27:13 -0500

    Modificación vista check list.

--- src/components/contents/sectionChecklist/Checklist.jsx
+++ src/components/contents/sectionChecklist/Checklist.jsx
@@ -5,91 +5,105 @@ import axios from 'axios'
 
 
 export default function Checklist(props) {
+  // const id_user = 1
+  const db = `https://api-fake-procrastin-app.vercel.app/users`
 
-   const db = 'https://api-fake-procrastin-app.vercel.app/usuarios'
+  /*  const [user, setUser] = useState({
+       id : "1",
+       nombre : "Gisell Mazo",
+       correo : "gisellmazo@gmail.com",
+       usuario : "gisellmazo13",
+       contraseña : "1214743656",
+       tareas : 
+           {
+               tarea1 : "Hacer tarea de Historia",
+               tarea2 : "Estudiar Inglés",
+               tarea3 : "Ejercitarme",
+               tarea4 : "Leer libro pendiente"
+           }
+   }) */
 
-    const [user, setUser] = useState({
-        id : "1",
-        nombre : "Gisell Mazo",
-        correo : "gisellmazo@gmail.com",
-        usuario : "gisellmazo13",
-        contraseña : "1214743656",
-        tareas : 
-            {
-                tarea1 : "Hacer tarea de Historia",
-                tarea2 : "Estudiar Inglés",
-                tarea3 : "Ejercitarme",
-                tarea4 : "Leer libro pendiente"
-            }
-        
-    })
+  const [data, setData] = useState([]);
 
-   /* const [database, setDatabase] = useState([]);
+  const peticionGet = async () => {
+    await axios.get(db)
+      .then(response => {
+        console.log(response)
+        setData(response.data)
+      })
+  }
 
-    const peticionGet = async () => {
-        await axios.get(db)
-            .then(response => {
-                setDatabase(response.database)
-            })
-    }
+  useEffect(async () => {
+    await peticionGet();
+  }, [])
 
-    useEffect(async () => {
-        await peticionGet();
-    }, [])*/
-
-    /*función en la que especificamos que sehara con la tarea seleccionada
-    dependiendo la acción, ya sea editar o borrar*/
-    /*const selectTask = (task, action) =>{
-        setTaksSelected(task);
-        (action === 'Editar')
-    }*/
-
-    /*const mostrarTarea = () =>{
-        fetch('https://api-fake-procrastin-app.vercel.app/usuarios')
-        .then(response => response.json())
-        .then(user => setUser(user))
-
-    }*/
-
-
-    return (
+  return (
+    <div className="col">
+      <h2 className="titleList mt-4 text-left">Lista de Tareas</h2>
+      <div className="mt-3 checklistBox">
         <div className="col">
-            <h2 className="titleList mt-4 text-left">Lista de Tareas</h2>
-            <div className="mt-3 checklistBox">
-                <div className="col">
 
-                    <ul>
-                        {/*{
-                            database.map((item) => {
-                                return(
-                                <div className="row">
-                                    <li className="col-8" key={item.id}>{item.nombre}<input className="checkbox" type="checkbox" /></li>
-                                    <ButtonsEditDelete />
-                                </div>
-                            )})
-                        }*/}
+          <ul>
+            {
+              data.map((item) => {
+                return (
+                  <div className="row">
+                    <li className="col-8" key={item.id}>{item.tasks.task1}<input className="checkbox" type="checkbox" /></li>
+                    <ButtonsEditDelete />
+                  </div>
+                )
+              })
+            }
 
+            {
+              data.map((item) => {
+                return (
+                  <div className="row">
+                    <li className="col-8" key={item.id}>{item.tasks.task2}<input className="checkbox" type="checkbox" /></li>
+                    <ButtonsEditDelete />
+                  </div>
+                )
+              })
+            }
 
-                        <div className="row">
-                            <li className="col-8">{user.tareas.tarea1}<input className="checkbox" type="checkbox" /></li>
-                            <ButtonsEditDelete />
-                        </div>
+            {
+              data.map((item) => {
+                return (
+                  <div className="row">
+                    <li className="col-8" key={item.id}>{item.tasks.task3}<input className="checkbox" type="checkbox" /></li>
+                    <ButtonsEditDelete />
+                  </div>
+                )
+              })
+            }
 
-                        <div className="row">
-                            <li className="col-8">{user.tareas.tarea2}<input className="checkbox" type="checkbox" /></li>
-                            <ButtonsEditDelete />
-                        </div>
+            {
+              data.map((item) => {
+                return (
+                  <div className="row">
+                    <li className="col-8" key={item.id}>{item.tasks.task4}<input className="checkbox" type="checkbox" /></li>
+                    <ButtonsEditDelete />
+                  </div>
+                )
+              })
+            }
 
-                        <div className="row">
-                            <li className="col-8">{user.tareas.tarea3}<input className="checkbox" type="checkbox" /></li>
-                            <ButtonsEditDelete />
-                        </div>
-                    </ul>
+            {/* <div className="row">
+                  <li className="col-8">{user.tareas.tarea1}<input className="checkbox" type="checkbox" /></li>
+                    <ButtonsEditDelete />
                 </div>
-            </div>
-
-
-
+                <div className="row">
+                  <li className="col-8">{user.tareas.tarea2}<input className="checkbox" type="checkbox" /></li>
+                    <ButtonsEditDelete />
+                </div>
+                <div className="row">
+                  <li className="col-8">{user.tareas.tarea3}<input className="checkbox" type="checkbox" /></li>
+                    <ButtonsEditDelete />
+                </div>
+            */}
+          </ul>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
