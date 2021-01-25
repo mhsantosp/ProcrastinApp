1ed0ce45fe52a2ca9afebb2b357db90d1169ed3e
Gisell Mazo <gisellmazo@gmail.com>
2021-01-24 23:00:00 -0500

    cambios

--- src/components/contents/sectionChecklist/Checklist.jsx
+++ src/components/contents/sectionChecklist/Checklist.jsx
@@ -6,25 +6,6 @@ import ButtonsEditDelete from './ButtonsEditDelete'
 
 export default function Checklist(props) {
   
-
-  /*  const [user, setUser] = useState({
-       id : "1",
-       nombre : "Gisell Mazo",
-       correo : "gisellmazo@gmail.com",
-       usuario : "gisellmazo13",
-       contraseña : "1214743656",
-       tareas : 
-           {
-               tarea1 : "Hacer tarea de Historia",
-               tarea2 : "Estudiar Inglés",
-               tarea3 : "Ejercitarme",
-               tarea4 : "Leer libro pendiente"
-           }
-   }) */
-
-  
-  
-
   return (
     <div className="col">
       <h2 className="titleList mt-4 text-left">Lista de Tareas</h2>
