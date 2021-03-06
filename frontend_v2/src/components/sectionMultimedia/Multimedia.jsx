fe069682d79f15575613a8416cf51d797d6cfce5
Maria Helena Santos Pineda <msantospineda1@gmail.com>
2021-03-06 02:13:41 -0500

    merge to gisell_mazo

---
+++ frontend_v2/src/components/sectionMultimedia/Multimedia.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Music from "../sectionMusic/SectionMusic";
+import Podcasts from "../sectionPodcasts/SectionPodcasts"
+import YoutubeVideos from "../sectionYoutube/SectionYoutube";
+
+export default function Multimedia(props) {
+  return (
+    <section className="container-fluid sectionMultimedia">
+      <nav>
+        <div className="nav nav-tabs" id="nav-tab" role="tablist">
+          <button className="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Spotify Playlists</button>
+          <button className="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Podcasts</button>
+          <button className="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Youtube Playlists</button>
+        </div>
+      </nav>
+      <div className="tab-content" id="nav-tabContent">
+        <div className="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+          <Music />
+        </div>
+        <div className="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+          <Podcasts />
+        </div>
+        <div className="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+          <YoutubeVideos />
+        </div>
+      </div>
+    </section>
+  )
+}
---
+++ frontend_v2/src/components/sectionYoutube/SectionYoutube.jsx
@@ -0,0 +1,168 @@
+import React from 'react';
+
+export default function SectionYoutube(props) {
+  return (
+    <section className="container-fluid sectionYoutubeVideos">
+      <h1 className="mt-5 titleYoutube">Youtube Playlists</h1>
+      <p className="mt-3 pYoutube">En la plataforma Youtube también puedes encontrar contenido que te puede ayudar a evitar los momentos de distracción tales como vídeos de larga duración con intrumentales y difrentes sonidos para diferentes situaciones, ya sea para trabajar, estudiar o relajarte por un rato.</p>
+
+      <div className="music col-12">
+        <div className="youtubeVideos">
+          <h3 className="titleVideos">Para escuchar mientras meditas o te relajas</h3>
+          <div className="row videos col-sm-12">
+            <div className="col-lg-4 col-sm-12 video">
+              <iframe width="253" title="video de youtube instrumental para meditar o relajarse" height="180" src="https://www.youtube.com/embed/FjHGZj2IjBk?list=WL" frameBorder="0" allow="accelerometer; controls; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+            </div>
+
+            <div className="col-lg-4 col-sm-12 video">
+              <iframe width="253" height="180" title="video de youtube instrumental para meditar o relajarse" src="https://www.youtube.com/embed/zjdiXz3iWr8" frameBorder="0" allow="accelerometer; controls; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
+
+            <div className="col-lg-4 col-sm-12 video">
+              <iframe width="253" height="180" title="video de youtube instrumental para meditar o relajarse" src="https://www.youtube.com/embed/zFhfksjf_mY" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
+          </div>
+
+          {/* NOT VISIBLE */}
+
+          <div className="row videos col-sm-12 collapse multi-collapse" id="multiCollapseExample2">
+            <div className="col-lg-4 col-sm-12 video">
+              <iframe width="253" height="180" title="video de youtube instrumental para meditar o relajarse" src="https://www.youtube.com/embed/JbSKfvO8u_M" frameBorder="0" allow="accelerometer; controls; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+            </div>
+
+            <div className="col-lg-4 col-sm-12 video">
+              <iframe width="253" height="180" title="video de youtube instrumental para meditar o relajarse" src="https://www.youtube.com/embed/WVmNReVHEiQ" frameBorder="0" allow="accelerometer; controls; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
+
+            <div className="col-lg-4 col-sm-12 video">
+              <iframe width="253" height="180" title="video de youtube instrumental para meditar o relajarse" src="https://www.youtube.com/embed/g8NVwN0_mks" frameBorder="0" allow="accelerometer; controls; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
+          </div>
+
+          <div className="row videos col-sm-12 collapse multi-collapse" id="multiCollapseExample2">
+            <div className="col-lg-4 col-sm-12 video">
+              <iframe width="253" height="180" title="video de youtube instrumental para meditar o relajarse" src="https://www.youtube.com/embed/ETnLewwT2qQ" frameBorder="0" allow="accelerometer; controls; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+            </div>
+
+            <div className="col-lg-4 col-sm-12 video">
+              <iframe width="253" height="180" title="video de youtube instrumental para meditar o relajarse" src="https://www.youtube.com/embed/lCOF9LN_Zxs" frameBorder="0" allow="accelerometer; controls; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
+
+            <div className="col-lg-4 col-sm-12 video">
+              <iframe width="253" height="180" title="video de youtube instrumental para meditar o relajarse" src="https://www.youtube.com/embed/NliYy7iqh-U" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
+          </div>
+
+          {/* NOT VISIBLE */}
+
+          <p>
+            <button className="btn btn-seemore" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Ver Más</button>
+          </p>
+        </div>
+
+        <div className="youtubeVideos">
+          <h3 className="titleVideos">Para escuchar mientras estudias</h3>
+          <div className="row videos col-sm-12">
+            <div className="col-lg-4 col-sm-12 video">
+              <iframe width="253" height="180" title="video de youtube instrumental para escuchar mientras estudias" src="https://www.youtube.com/embed/-FlxM_0S2lA" frameBorder="0" allow="accelerometer; controls; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+            </div>
+
+            <div className="col-lg-4 col-sm-12 video">
+              <iframe width="253" height="180" title="video de youtube instrumental para escuchar mientras estudias" src="https://www.youtube.com/embed/-zS4_iMmSfw" frameBorder="0" allow="accelerometer; controls; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
+
+            <div className="col-lg-4 col-sm-12 video">
+              <iframe width="253" height="180" title="video de youtube instrumental para escuchar mientras estudias" src="https://www.youtube.com/embed/wAPCSnAhhC8" frameBorder="0" allow="accelerometer; controls; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
+          </div>
+
+          {/* NOT VISIBLE */}
+
+          <div className="row videos col-sm-12 collapse multi-collapse" id="multiCollapseExample3">
+            <div className="col-lg-4 col-sm-12 video">
+              <iframe width="253" height="180" title="video de youtube instrumental para escuchar mientras estudias" src="https://www.youtube.com/embed/mUeZDu9rBH8?list=WL" frameBorder="0" allow="accelerometer; controls; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+            </div>
+
+            <div className="col-lg-4 col-sm-12 video">
+              <iframe width="253" height="180" title="video de youtube instrumental para escuchar mientras estudias" src="https://www.youtube.com/embed/IJ0kurZy1DE?list=WL" frameBorder="0" allow="accelerometer; controls; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
+
+            <div className="col-lg-4 col-sm-12 video">
+              <iframe width="253" height="180" title="video de youtube instrumental para escuchar mientras estudias" src="https://www.youtube.com/embed/uw_elzX7Oo4" frameBorder="0" allow="accelerometer; controls; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
+          </div>
+
+          <div className="row videos col-sm-12 collapse multi-collapse" id="multiCollapseExample3">
+            <div className="col-lg-4 col-sm-12 video">
+              <iframe width="253" height="180" title="video de youtube instrumental para escuchar mientras estudias" src="https://www.youtube.com/embed/8-6ltMNZsZU" frameBorder="0" allow="accelerometer; controls; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+            </div>
+
+            <div className="col-lg-4 col-sm-12 video">
+              <iframe width="253" height="180" title="video de youtube instrumental para escuchar mientras estudias" src="https://www.youtube.com/embed/XXGG39bHQi4" frameBorder="0" allow="accelerometer; controls; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
+
+            <div className="col-lg-4 col-sm-12 video">
+              <iframe width="253" height="180" title="video de youtube instrumental para escuchar mientras estudias" src="https://www.youtube.com/embed/HHezNj2XQ2A" frameBorder="0" allow="accelerometer; controls; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
+          </div>
+
+          {/* NOT VISIBLE */}
+
+          <p>
+            <button className="btn btn-seemore" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample3" aria-expanded="false" aria-controls="multiCollapseExample3">Ver Más</button>
+          </p>
+
+        </div>
+
+        <div className="youtubeVideos">
+          <h3 className="titleVideos">Para escuchar mientras programas</h3>
+          <div className="row videos col-sm-12">
+            <div className="col-lg-4 col-sm-12 video">
+              <iframe width="253" height="180" title="video de youtube instrumental para escuchar mientras programas" src="https://www.youtube.com/embed/f02mOEt11OQ" frameBorder="0" allow="accelerometer; controls; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+            </div>
+
+            <div className="col-lg-4 col-sm-12 video">
+              <iframe width="253" height="180" title="video de youtube instrumental para escuchar mientras programas" src="https://www.youtube.com/embed/xTgee06CgLg" frameBorder="0" allow="accelerometer; controls; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
+
+            <div className="col-lg-4 col-sm-12 video">
+              <iframe width="253" height="180" title="video de youtube instrumental para escuchar mientras programas" src="https://www.youtube.com/embed/M5QY2_8704o" frameBorder="0" allow="accelerometer; controls; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
+          </div>
+
+          {/* NOT VISIBLE */}
+          <div className="row videos col-sm-12 collapse multi-collapse" id="multiCollapseExample4">
+            <div className="col-lg-4 col-sm-12 video">
+              <iframe width="253" height="180" title="video de youtube instrumental para escuchar mientras programas" src="https://www.youtube.com/embed/0uhwPOquByE" frameBorder="0" allow="accelerometer; controls; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+            </div>
+
+            <div className="col-lg-4 col-sm-12 video">
+              <iframe width="253" height="180" title="video de youtube instrumental para escuchar mientras programas" src="https://www.youtube.com/embed/M7g7Pfx6zjg" frameBorder="0" allow="accelerometer; controls; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
+
+            <div className="col-lg-4 col-sm-12 video">
+              <iframe width="253" height="180" title="video de youtube instrumental para escuchar mientras programas" src="https://www.youtube.com/embed/wTvkT6Q_CwI" frameBorder="0" allow="accelerometer; controls; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
+          </div>
+
+          {/* NOT VISIBLE */}
+          <p>
+            <button className="btn btn-seemore" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample4" aria-expanded="false" aria-controls="multiCollapseExample4">Ver Más</button>
+          </p>
+
+        </div>
+
+
+
+      </div>
+    </section>
+  )
+}
