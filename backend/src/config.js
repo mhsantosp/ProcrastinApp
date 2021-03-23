4f4599cf992bf62bbd6a570e0aaffbef372d4046
Gisell Mazo <gisellmazo@gmail.com>
2021-03-23 08:47:28 -0500

    error merge

--- backend/src/config.js
+++ backend/src/config.js
@@ -3,6 +3,6 @@
 
 export default {
   MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://gisellmazo:1214743656@cluster0.flrhc.mongodb.net/procrastinapp-db?retryWrites=true&w=majority",
-  PORT: process.env.PORT || 4001,
+  PORT: process.env.PORT || 4000,
   SECRET: 'prueba-back'
 }
