0e4a56c42dccc36f75319a99ddee7a65ccf05a41
Gisell Mazo <gisellmazo@gmail.com>
2021-03-24 23:04:23 -0500

    cambio config

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,6 +1,3 @@
-// import { config } from "dotenv";
-// config();
-
 export default {
   MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://gisellmazo:1214743656@cluster0.flrhc.mongodb.net/procrastinapp-db?retryWrites=true&w=majority",
   PORT: process.env.PORT || 4000,
